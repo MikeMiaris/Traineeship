09c665dbdab112b518adde60322dc27c9f13bfaf
Michalis Miaris <mikemiaris97@gmail.com>
2025-05-13 22:34:37 +0300

    hmm

--- src/main/java/com/example/traineeship/services/ProfessorServiceImpl.java
+++ src/main/java/com/example/traineeship/services/ProfessorServiceImpl.java
@@ -1,5 +1,63 @@
 package com.example.traineeship.services;
 
-public class ProfessorServiceImpl {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.traineeship.domainmodel.Evaluation;
+import com.example.traineeship.domainmodel.Professor;
+import com.example.traineeship.domainmodel.TraineeshipPosition;
+import com.example.traineeship.mappers.CompanyMapper;
+import com.example.traineeship.mappers.EvaluationMapper;
+import com.example.traineeship.mappers.ProfessorMapper;
+import com.example.traineeship.mappers.TraineeshipPositionMapper;
+
+public class ProfessorServiceImpl implements ProfessorService{
+
+    @Autowired
+    private ProfessorMapper professorMapper;
+
+    @Autowired
+    private TraineeshipPositionMapper positionMapper;
+
+    @Autowired
+    private EvaluationMapper evaluationMapper;
+
+	@Override
+	public Professor retreiveProfile(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveProfile(Professor professor) {
+		professorMapper.save(professor);
+		
+	}
+
+	@Override
+	public List<TraineeshipPosition> retrieveAssignedPositions() {
+        List<Professor> allProfessors = professorMapper.findAll();
+        List<TraineeshipPosition> result = new ArrayList<>();
+
+        for (Professor prof : allProfessors) {
+            result.addAll(prof.getSupervisedPositions());
+        }
+        return result;
+	}
+
+	@Override
+	public void evaluateAssignedPosition(Integer position) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveEvaluation(Integer positionId, Evaluation evaluation) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
