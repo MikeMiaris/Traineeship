66c857abdbddab3b8d943a6f40cb84cdc0df669a
Dimos Apostolidis <92276118+dimos789@users.noreply.github.com>
2025-05-15 18:26:52 +0300

    comments

--- src/main/java/com/example/traineeship/domainmodel/Role.java
+++ src/main/java/com/example/traineeship/domainmodel/Role.java
@@ -1,5 +1,7 @@
 package com.example.traineeship.domainmodel;
 
+// D: Check requirements definition, committee members don't have accounts?
+// seems odd but checks out with UML
 public enum Role {
 	STUDENT, COMPANY, PROFESSOR, COMMITTEE
 }
