128795909509a19499ed1a00062cfa572f2b98bc
Dimos Apostolidis <92276118+dimos789@users.noreply.github.com>
2025-05-16 21:35:27 +0300

    small change

--- src/main/java/com/example/traineeship/services/CompanyService.java
+++ src/main/java/com/example/traineeship/services/CompanyService.java
@@ -13,6 +13,7 @@ public interface CompanyService {
 	List<TraineeshipPosition> retrieveAvailablePositions(String username);
 	void addPosition(String username, TraineeshipPosition position);
 	List<TraineeshipPosition> retreiveAssignedPositions(String username);
+	//void deleteAssignedPosition(Integer positionId); -- DON'T NEED? TODO
 	void evaluateAssignedPosition(Integer positionId);
 	void saveEvaluation(Integer positionId, Evaluation evaluation);
 }
