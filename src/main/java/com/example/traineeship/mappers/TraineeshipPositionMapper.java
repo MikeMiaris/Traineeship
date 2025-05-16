da2e1cd83e123a2304ccccea491f5d404c3c4699
EliasGeorgiadis <92232934+EliasGeorgiadis@users.noreply.github.com>
2025-05-16 20:23:39 +0300

    Update TraineeshipPositionMapper.java

--- src/main/java/com/example/traineeship/mappers/TraineeshipPositionMapper.java
+++ src/main/java/com/example/traineeship/mappers/TraineeshipPositionMapper.java
@@ -1,16 +1,31 @@
-package com.example.traineeship.services;
+package com.example.traineeship.mappers;
 
-public interface CommitteeService {
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.example.traineeship.domainmodel.TraineeshipPosition;
-import com.example.traineeship.domainmodel.Student;
 
-public interface CommitteeService {
-	List<TraineeshipPosition> retrievePositionsForApplicant(String applicantUsername, String Strateg);
-	List<Student> retrieveTraineeShipApplications();
-	void assignPosition(Integer positionid, String studentUsername);
-	void assignSupervisor(Integer positionid, String strategy);
-	List<TraineeshipPosition> listAssignedTraineeships();
-	void completeAssignedTraineeships(Integer positionid);
+@Repository
+public interface TraineeshipPositionMapper extends JpaRepository<TraineeshipPosition, Integer>{
+
+	Optional<TraineeshipPosition> findById(Integer positionId);
+	
+	@Query("Select p FROM TraineeshipPosition p WHERE p.company.companyLocation = :location")
+	static
+	List<TraineeshipPosition> findByLocation(@Param("location") String location) {
+		return null;
+	}
+	
+	
+	@Query("Select p FROM TraineeshipPosition p WHERE p.topics = :topic")
+	static
+	List<TraineeshipPosition> findBytopic(@Param("topic") String topic) {
+		return null;
+	}
+	
 }
