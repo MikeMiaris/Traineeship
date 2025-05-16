0ff7a2f5bb089b88263262f31b837a134054cf8a
Dimos Apostolidis <92276118+dimos789@users.noreply.github.com>
2025-05-16 21:37:55 +0300

    just it

--- src/main/java/com/example/traineeship/mappers/CompanyMapper.java
+++ src/main/java/com/example/traineeship/mappers/CompanyMapper.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.traineeship.domainmodel.Company;
 
+// D: changed to reflect that Company's primary key is String
 public interface CompanyMapper extends JpaRepository<Company,String>{
-	 Company findByUsername(String username);
+
 }
