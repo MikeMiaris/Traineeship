0eeb24ccd0775b37de6722c215557e30c2b263dc
EliasGeorgiadis <92232934+EliasGeorgiadis@users.noreply.github.com>
2025-05-16 20:24:51 +0300

    Update CommitteeServiceImpl.java

--- src/main/java/com/example/traineeship/services/CommitteeServiceImpl.java
+++ src/main/java/com/example/traineeship/services/CommitteeServiceImpl.java
@@ -1,5 +1,35 @@
 package com.example.traineeship.services;
 
-public class CommitteeServiceImpl {
+import java.util.List;
 
+import com.example.traineeship.domainmodel.Student;
+import com.example.traineeship.domainmodel.TraineeshipPosition;
+
+public class CommitteeServiceImpl {
+	
+	
+	
+	List<TraineeshipPosition> retrievePositionsForApplicant(String applicantUsername, String Strategy){
+		
+		
+	}
+	List<Student> retrieveTraineeShipApplications(){
+		
+		
+	}
+	void assignPosition(Integer positionid, String studentUsername){
+		
+		
+	}
+	void assignSupervisor(Integer positionid, String strategy){
+		
+		
+	}
+	List<TraineeshipPosition> listAssignedTraineeships(){
+		
+		
+	}
+	void completeAssignedTraineeships(Integer positionid) {
+		
+	}
 }
