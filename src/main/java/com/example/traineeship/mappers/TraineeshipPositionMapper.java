6c6566a73e791ca53918cee109720705a2dcb324
EliasGeorgiadis <92232934+EliasGeorgiadis@users.noreply.github.com>
2025-05-16 20:22:35 +0300

    Update TraineeshipPositionMapper.java

--- src/main/java/com/example/traineeship/mappers/TraineeshipPositionMapper.java
+++ src/main/java/com/example/traineeship/mappers/TraineeshipPositionMapper.java
@@ -1,13 +1,32 @@
 package com.example.traineeship.mappers;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.example.traineeship.domainmodel.TraineeshipPosition;
 
+public interface TraineeshipPositionMapper extends JpaRepository<TraineeshipPosition, Long>{
+@Repository
 public interface TraineeshipPositionMapper extends JpaRepository<TraineeshipPosition, Integer>{
 
 	Optional<TraineeshipPosition> findById(Integer positionId);
+	
+	@Query("Select p FROM TraineeshipPosition p WHERE p.company.companyLocation = :location")
+	static
+	List<TraineeshipPosition> findByLocation(@Param("location") String location) {
+		return null;
+	}
+	
+	
+	@Query("Select p FROM TraineeshipPosition p WHERE p.topics = :topic")
+	static
+	List<TraineeshipPosition> findBytopic(@Param("topic") String topic) {
+		return null;
+	}
 	
 }
