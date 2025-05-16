7d194791ea2832b7520eeaede604683b85946609
EliasGeorgiadis <92232934+EliasGeorgiadis@users.noreply.github.com>
2025-05-16 20:54:45 +0300

    namefix

--- src/main/java/com/example/traineeship/factories/PositionSearchFactory.java
+++ src/main/java/com/example/traineeship/factories/PositionSearchFactory.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PositionsSearchFactory {
+public class PositionSearchFactory {
     @Autowired
     private SearchBasedOnLocation searchBasedOnLocation;
     
