a1a1fcbd08fcf5f98bf1284888be0cde32289b80
EliasGeorgiadis <92232934+EliasGeorgiadis@users.noreply.github.com>
2025-05-16 20:24:32 +0300

    Update CommitteeService.java

--- src/main/java/com/example/traineeship/services/CommitteeService.java
+++ src/main/java/com/example/traineeship/services/CommitteeService.java
@@ -1,5 +1,15 @@
 package com.example.traineeship.services;
 
-public interface CommitteeService {
+import java.util.List;
 
+import com.example.traineeship.domainmodel.TraineeshipPosition;
+import com.example.traineeship.domainmodel.Student;
+
+public interface CommitteeService {
+	List<TraineeshipPosition> retrievePositionsForApplicant(String applicantUsername, String Strateg);
+	List<Student> retrieveTraineeShipApplications();
+	void assignPosition(Integer positionid, String studentUsername);
+	void assignSupervisor(Integer positionid, String strategy);
+	List<TraineeshipPosition> listAssignedTraineeships();
+	void completeAssignedTraineeships(Integer positionid);
 }
