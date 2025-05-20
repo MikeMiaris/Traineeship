0d9dab38041774e1e14346f56bc963cf23b6408f
Dimos Apostolidis <92276118+dimos789@users.noreply.github.com>
2025-05-20 22:55:01 +0300

    likely final

--- src/main/java/com/example/traineeship/controllers/CompanyController.java
+++ src/main/java/com/example/traineeship/controllers/CompanyController.java
@@ -22,7 +22,11 @@ import com.example.traineeship.services.UserService;
 public class CompanyController {
 	
 	// D: again, a lot of those taken directly from example
-	// we'll have to see what's needed
+	
+	// I'm changing evaluateAssignedTraineeship, it just makes sense to check the correlation only in service
+	// so pass username as argument for the love of god
+	
+	
 	@Autowired
 	UserService userService;
 	
@@ -38,11 +42,14 @@ public class CompanyController {
 	public String retrieveProfile(Model model) {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();	
 		String compUsername = authentication.getName();
-		System.err.println("XXXXXX " + compUsername);
-        Company company = companyService.retrieveProfile(compUsername);
-        
-        model.addAttribute("company", company);
-        
+		try {
+			Company company = companyService.retrieveProfile(compUsername);
+			model.addAttribute("company", company);
+		}catch(Exception e){
+			model.addAttribute("error", e.getMessage());
+			return "redirect:/company/dashboard";
+		}
+		
         return "company/company-profile";
 	}
 	
@@ -58,14 +65,103 @@ public class CompanyController {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		
 		String username = authentication.getName();
-		System.err.println("Username " + username);
 		
-		List<TraineeshipPosition> positions = companyService.retrieveAvailablePositions(username);
+		try {
+			List<TraineeshipPosition> positions = companyService.retrieveAvailablePositions(username);
+			model.addAttribute("available_positions", positions);
+		}catch(Exception e) {
+			model.addAttribute("error", e.getMessage());
+			return "redirect:/company/dashboard";
+		}
 		
-		model.addAttribute("available_positions", positions);
 		
 		return "company/available_positions";
 		
 	}
-	// D: go from showpositionform onwards here
+	
+	@RequestMapping("/company/show_position_form")
+	public String showPositionForm(Model model) {
+		
+		TraineeshipPosition position = new TraineeshipPosition();
+		model.addAttribute("position", position);		
+		
+		return "company/position-form";
+	}
+	
+	@RequestMapping("/company/save_position")
+	public String savePosition(@ModelAttribute("position") TraineeshipPosition position, Model model) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String username = authentication.getName();
+		try {
+			companyService.addPosition(username, position);
+			return "redirect:/company/list_positions";
+		}catch(Exception e) {
+			model.addAttribute("error", e.getMessage());
+			return "redirect:/company/dashboard";
+		}
+		
+	}
+	
+	@RequestMapping("/company/list_assigned_positions")
+	public String listAssignedPositions(Model model) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String username = authentication.getName();
+		try {
+			List <TraineeshipPosition> positions = companyService.retrieveAssignedPositions(username);
+			model.addAttribute("positions", positions);
+			return "company/assigned_positions";
+		}catch(Exception e){
+			model.addAttribute("error", e.getMessage());
+			return "redirect:/company/dashboard";
+		}
+		
+	}
+	
+	// D:  this will probably set you on the path of evaluating traineeship
+	// authentications in service doesn't make sense to me, so maybe by passing position_id
+	// we can access that mapper in company service
+	@RequestMapping("/company/evaluate_assigned_traineeship")
+	public String evaluateAssignedTraineeship(@ModelAttribute("position_id") Integer positionId, Model model) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String username = authentication.getName();
+		
+		// D: check if company can evaluate position, stop otherwise?
+		try {
+			companyService.evaluateAssignedPosition(username, positionId);
+			model.addAttribute("evaluation", new Evaluation());
+			return "company/evaluation-form";
+		}
+		// the way errors are to be handled ig, although dunno if this is somehow even viable
+		catch (Exception e) {
+			model.addAttribute("error", e.getMessage());
+			return "redirect:/company/list_assigned_positions";
+		}
+	}
+	
+	// D: now, with an evaluation all set, get to saving
+	@RequestMapping("/company/save_evaluation")
+	public String saveEvaluation(@ModelAttribute("position_id") Integer positionId,
+								@ModelAttribute("evaluation") Evaluation evaluation, Model model) {
+		try {
+			companyService.saveEvaluation(positionId, evaluation);
+			return "redirect:/company/list_assigned_positions";
+		}catch(Exception e) {
+			model.addAttribute("error", e.getMessage());
+			return "redirect:/company/list_assigned_positions";
+		}
+		
+	}
+	
+	@RequestMapping("/company/delete_position")
+	public String deletePosition(@ModelAttribute("position_id") Integer positionId, Model model) {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String username = authentication.getName();
+		try {
+			companyService.deleteAssignedPosition(username, positionId);
+			return "redirect:/company/list_assigned_positions";
+		}catch(Exception e) {
+			model.addAttribute("error", e.getMessage());
+			return "redirect:/company/list_assigned_positions";
+		}
+	}
 }
