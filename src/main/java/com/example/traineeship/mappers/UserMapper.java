14fef6ebba980fc58a5395566351f70f6ebe4b43
Dimos Apostolidis <92276118+dimos789@users.noreply.github.com>
2025-05-22 18:05:44 +0300

    it ok???

--- src/main/java/com/example/traineeship/mappers/UserMapper.java
+++ src/main/java/com/example/traineeship/mappers/UserMapper.java
@@ -1,9 +1,11 @@
 package com.example.traineeship.mappers;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.traineeship.domainmodel.User;
 
-public interface UserMapper extends JpaRepository<User, String>{
-
+public interface UserMapper extends JpaRepository<User, Integer>{
+	Optional<User> findByUsername(String username);
 }
