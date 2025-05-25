d097f181ac68a917b7bac36280f623b4b76d3c30
Dimos Apostolidis <92276118+dimos789@users.noreply.github.com>
2025-05-25 14:01:19 +0300

    Add files via upload

---
+++ src/main/java/com/example/traineeship/config/WebSecurityConfig.java
@@ -0,0 +1,92 @@
+package com.example.traineeship.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import com.example.traineeship.services.UserServiceImpl;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    private CustomSecuritySuccessHandler customSecuritySuccessHandler;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+    	UserDetailsService ret = new UserServiceImpl();
+    	return ret;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authenticationProvider(authenticationProvider())  
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/user/**").permitAll()
+                .requestMatchers("/student/**").hasRole("STUDENT")
+                .requestMatchers("/professor/**").hasRole("PROFESSOR")
+                .requestMatchers("/company/**").hasRole("COMPANY")
+                .requestMatchers("/committee/**").hasRole("COMMITTEE") 
+
+                .anyRequest().authenticated()
+            )
+            .formLogin(form -> form
+                .loginPage("/login")
+                .successHandler(customSecuritySuccessHandler)
+                .failureUrl("/login?error=true")
+                .permitAll()
+            )
+            .logout(logout -> logout
+            	    .logoutUrl("/logout")
+            	    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+            	    .logoutSuccessUrl("/")
+            	)
+            .exceptionHandling((exception) -> 
+            	exception
+                .accessDeniedHandler((request, response, accessDeniedException) -> 
+                    response.sendRedirect("/access-denied")
+                )
+        );
+        return http.build();
+    }
+
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
+    }
+
+}
